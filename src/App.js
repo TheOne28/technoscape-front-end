4e1ecd233149b6a7bae1eb6eba38920f98ad65eb
Yosua G <yosua.gunawan002@binus.ac.id>
2022-06-18 20:26:54 +0700

    Tambah reactstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {tombol} from './Komponen/try';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Cool <code>src/App.js</code> and save to reload.
         </p>
+        <tombol/>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Komponen/try.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+
+export function tombol (props) {
+  return (
+    <Button color="danger">Danger!</Button>
+  );
+};
